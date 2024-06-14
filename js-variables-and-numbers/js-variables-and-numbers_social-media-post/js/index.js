508d3d4b3f2e4600d879eddbe5f8f62e7b1fa587
Melanie Adler <adl.melanie.95@gmail.com>
2024-06-14 11:59:21 +0200

    add js variables and numbers solutions

---
+++ js-variables-and-numbers/js-variables-and-numbers_social-media-post/js/index.js
@@ -0,0 +1,37 @@
+console.clear();
+
+/*
+1. Create the data for a small social media post. Create a const variable for each data point listed below.:
+	- a title
+	- a text
+	- the number of likes
+	- the user who created the post
+	- a boolean called isReported
+*/
+
+// --v-- write your code here --v--
+
+const title = "Look at my new post";
+const text =
+  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident eligendi eaque voluptatibus veritatis ad.";
+let numberOfLikes = 12;
+const userName = "Jane Doe";
+const isReported = true;
+
+// --^-- write your code here --^--
+
+/*
+2. Log all variables to the console. Increase the likes by one and log the amount of likes again. Adapt your code from 1.1 if necessary.
+*/
+
+// --v-- write your code here --v--
+
+console.log(title);
+console.log(text);
+console.log(numberOfLikes);
+numberOfLikes++;
+console.log(numberOfLikes);
+console.log(userName);
+console.log(isReported);
+
+// --^-- write your code here --^--
