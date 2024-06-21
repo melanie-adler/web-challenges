35aee20be52b1a5a4c393adb110af45fc5edb617
Melanie Adler <adl.melanie.95@gmail.com>
2024-06-21 11:43:19 +0200

    add solution excercise 3

--- js-functions-2/js-functions-2_arrow-functions/js/index.js
+++ js-functions-2/js-functions-2_arrow-functions/js/index.js
@@ -5,9 +5,10 @@ Rewrite the following functions as arrow functions.
 Use implicit returns when possible
 */
 
-function getCurrentHour() {
-  const now = new Date();
-  const currentHour = now.getHours();
+const now = new Date();
+const currentHour = now.getHours();
+
+const getCurrentHour = (currentHour) => {
   if (currentHour === 0) {
     return "12am";
   } else if (currentHour === 12) {
@@ -17,21 +18,26 @@ function getCurrentHour() {
   } else {
     return currentHour - 12 + "pm";
   }
-}
+};
+console.log(getCurrentHour(currentHour));
 
-function getVectorLength(x, y, z) {
+const getVectorLength = (x, y, z) => {
   return (x ** 2 + y ** 2 + z ** 2) ** 0.5;
-}
+};
 
-function cleanInput(string) {
+console.log(getVectorLength(1, 2, 3));
+
+const cleanInput = (string) => {
   return string.toLowerCase().trim();
-}
+};
+
+console.log(cleanInput("HIDE"));
 
 /*
 Rewrite the following arrow functions as classic functions.
 */
 
-const isOddAndSmall = (number) => {
+function isOddAndSmall(number) {
   if (number > 10) {
     return false;
   }
@@ -41,8 +47,18 @@ const isOddAndSmall = (number) => {
   }
 
   return true;
-};
+}
 
-const add3 = (a, b, c) => a + b + c;
+console.log(isOddAndSmall(7));
 
-const repeat10 = (string) => string.repeat(10);
+function add3(a, b, c) {
+  return a + b + c;
+}
+
+console.log(add3(2, 4, 6));
+
+function repeat10(string) {
+  return string.repeat(10);
+}
+
+console.log(repeat10("repeat"));
