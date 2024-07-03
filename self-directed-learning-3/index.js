63336fa1cf08fb7787e4976b679066f7f6e22356
Melanie Adler <adl.melanie.95@gmail.com>
2024-07-03 11:36:33 +0200

    add Full Name Generator

--- self-directed-learning-3/index.js
+++ self-directed-learning-3/index.js
@@ -1,8 +1,10 @@
 console.clear();
 
-const form = document.querySelector('[data-js="mail-generator-form"]');
+const mailGeneratorForm = document.querySelector(
+  '[data-js="mail-generator-form"]'
+);
 
-form.addEventListener("submit", (event) => {
+mailGeneratorForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const formData = new FormData(event.target);
@@ -17,5 +19,41 @@ function generateEmail(param1, param2) {
   );
 
   generatedMailOutput.textContent =
-    `${param1}.${param2}@example.com`.toLowerCase();
+    `Email: ${param1}.${param2}@example.com`.toLowerCase();
+}
+
+const fullNameGeneratorForm = document.querySelector(
+  '[data-js="full-name-generator-form"]'
+);
+
+fullNameGeneratorForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+
+  getUserFromEmail(data.email);
+});
+
+function getUserFromEmail(email) {
+  const generatedNameOutput = document.querySelector(
+    '[data-js="generated-full-name"]'
+  );
+
+  if (email.includes("@")) {
+    const fields = email.split("@");
+    const fullName = fields[0];
+    if (fullName.includes(".")) {
+      const nameToSplit = fullName.split(".");
+      const generatedFirstName = nameToSplit[0];
+      const generatedLastName = nameToSplit[1];
+      generatedNameOutput.textContent = `${generatedFirstName} ${generatedLastName}`;
+    } else {
+      console.log("Email doesn't include .");
+      return 0;
+    }
+  } else {
+    console.log("Email doesn't include @");
+    return 0;
+  }
 }
