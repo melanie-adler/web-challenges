9a54518af850064b4b0b6fcdc63cc1aeb3b013a9
Melanie Adler <adl.melanie.95@gmail.com>
2024-07-04 11:53:49 +0200

    add solution excercise 3

--- js-array-methods-1/js-array-methods_filter/js/index.js
+++ js-array-methods-1/js-array-methods_filter/js/index.js
@@ -24,13 +24,32 @@ const cards = [
   },
 ];
 
-const onlyCardWithIdTwo = null;
+const onlyCardWithIdTwo = cards.filter((card) => {
+  return card.id.includes("2");
+});
+// console.log(onlyCardWithIdTwo);
 
-const allCardsWith3Tags = null;
+const allCardsWith3Tags = cards.filter((card) => {
+  return card.tags.length === 3;
+});
+// console.log(allCardsWith3Tags);
 
-const allCardsThatAreNotBookmarked = null;
+const allCardsThatAreNotBookmarked = cards.filter((card) => {
+  return card.isBookmarked != true;
+});
+// console.log(allCardsThatAreNotBookmarked);
 
-const allCardsWithTagsHTMLOrJSThatAreBookmarked = null;
+const allCardsWithTagsHTMLOrJSThatAreBookmarked = cards.filter((card) => {
+  if (
+    (card.tags.includes("html") || card.tags.includes("js")) &&
+    card.isBookmarked === true
+  ) {
+    return card;
+  } else {
+    return 0;
+  }
+});
+console.log(allCardsWithTagsHTMLOrJSThatAreBookmarked);
 
 export {
   onlyCardWithIdTwo,
