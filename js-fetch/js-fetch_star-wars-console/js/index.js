68418420b1e4f03f97dfa4c4995d6497c7883a00
Melanie Adler <adl.melanie.95@gmail.com>
2024-07-09 11:52:40 +0200

    update solution excercise 1

--- js-fetch/js-fetch_star-wars-console/js/index.js
+++ js-fetch/js-fetch_star-wars-console/js/index.js
@@ -5,13 +5,9 @@ const url = "https://swapi.py4e.com/api/people";
 async function fetchData() {
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data.results[2].eye_color);
   return data;
 }
 
-console.log(fetchData());
-fetchData();
-
-// fetchData().then((value) => {
-//   console.log(value.results);
-// });
+fetchData().then((value) => {
+  console.log(value.results[2].eye_color);
+});
