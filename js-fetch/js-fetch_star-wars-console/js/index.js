98e3b2676570bb318bd60d6db6e58454255d37a9
Melanie Adler <adl.melanie.95@gmail.com>
2024-07-09 10:58:43 +0200

    add update solution excercise 1

--- js-fetch/js-fetch_star-wars-console/js/index.js
+++ js-fetch/js-fetch_star-wars-console/js/index.js
@@ -6,8 +6,10 @@ async function fetchData() {
   const response = await fetch(url);
   const data = await response.json();
   console.log(data.results[2].eye_color);
+  return data;
 }
 
+console.log(fetchData());
 fetchData();
 
 // fetchData().then((value) => {
