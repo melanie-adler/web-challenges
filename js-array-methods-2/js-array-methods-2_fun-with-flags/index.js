ee5feb085d72b9df355b62a2723ec46a661e8892
Melanie Adler <adl.melanie.95@gmail.com>
2024-07-11 16:55:42 +0200

    add solution excercise 7 part 2

--- js-array-methods-2/js-array-methods-2_fun-with-flags/index.js
+++ js-array-methods-2/js-array-methods-2_fun-with-flags/index.js
@@ -7,14 +7,28 @@ const queryInput = document.querySelector('[data-js="query-input"]');
 queryInput.addEventListener("input", (event) => {
   container.innerHTML = "";
 
-  const searchString = event.target.value;
+  const searchString =
+    event.target.value[0].toUpperCase() +
+    event.target.value.slice(1).toLowerCase();
 
-  const foundCountry = countries.find((country) =>
-    country.name.startsWith(searchString)
-  );
+  //   const foundCountry = countries.find((country) =>
+  //     country.name.startsWith(searchString)
+  //   );
 
-  if (foundCountry) {
-    const countryElement = Country(foundCountry);
-    container.append(countryElement);
-  }
+  //   if (foundCountry) {
+  //     const countryElement = Country(foundCountry);
+  //     container.append(countryElement);
+  //   }
+  // });
+
+  const foundCountries = countries.filter((country) => {
+    return country.name.startsWith(searchString);
+  });
+
+  foundCountries.forEach((country) => {
+    if (foundCountries) {
+      const countryElement = Country(country);
+      container.append(countryElement);
+    }
+  });
 });
