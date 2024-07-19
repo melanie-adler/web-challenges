520e90307a473251d40665eeb48a700ca5e787bd
Melanie Adler <adl.melanie.95@gmail.com>
2024-07-19 11:29:18 +0200

    add solution part 2/3 excercise 4

--- react/react-props/react-props_button/src/App.js
+++ react/react-props/react-props_button/src/App.js
@@ -2,19 +2,32 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
+  function handleClick() {
+    console.log("You clicked a button!");
+  }
+
   return (
     <>
-      <Button text="Button" color="red" />
-      <Button text="Another Button" color="green" disabled />
-      <Button text="Yet another Button" color="blue" />
-      <Button text="Fourth Button" color="yellow" />
+      <Button text="Button" color="red" onClick={handleClick} />
+      <Button
+        text="Another Button"
+        color="green"
+        onClick={handleClick}
+        disabled
+      />
+      <Button text="Yet another Button" color="blue" onClick={handleClick} />
+      <Button text="Fourth Button" color="yellow" onClick={handleClick} />
     </>
   );
 }
 
-function Button({ color, disabled, text }) {
+function Button({ color, disabled, text, onClick }) {
   return (
-    <button style={{ backgroundColor: color }} disabled={disabled}>
+    <button
+      style={{ backgroundColor: color }}
+      disabled={disabled}
+      onClick={onClick}
+    >
       {text}
     </button>
   );
