9366334678365d54c3f2fa3678d82a18aaeedbdf
Melanie Adler <adl.melanie.95@gmail.com>
2024-08-30 11:51:07 +0200

    update volume mapping

--- nextjs/nextjs-dynamic-routes_lotr-app-dynamic-routes/pages/volumes/index.js
+++ nextjs/nextjs-dynamic-routes_lotr-app-dynamic-routes/pages/volumes/index.js
@@ -14,25 +14,18 @@ export default function Volumes() {
 
     router.push(`/volumes/${randomVolume.slug}`);
   }
+  const { slug } = router.query;
 
   return (
     <>
       <h1>The Lord of the Rings</h1>
       <p>{introduction}</p>
       <ul>
-        <li>
-          <Link href="/volumes/the-fellowship-of-the-ring">
-            The Fellowship of the Ring
-          </Link>
-        </li>
-        <li>
-          <Link href="/volumes/the-two-towers">The Two Towers</Link>
-        </li>
-        <li>
-          <Link href="/volumes/the-return-of-the-king">
-            The Return of the King
-          </Link>
-        </li>
+        {volumes.map((volume) => (
+          <li key={volume.id}>
+            <Link href={`/volumes/${volume.slug}`}>{volume.title}</Link>
+          </li>
+        ))}
       </ul>
       <button type="button" onClick={handleClick}>
         Random Volume
