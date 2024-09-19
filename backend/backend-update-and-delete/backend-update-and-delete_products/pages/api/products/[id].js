d7f80ac29f087d2f7fead4f80163bc12402dabc2
Melanie Adler <adl.melanie.95@gmail.com>
2024-09-19 11:31:19 +0200

    add error messaging

--- backend/backend-update-and-delete/backend-update-and-delete_products/pages/api/products/[id].js
+++ backend/backend-update-and-delete/backend-update-and-delete_products/pages/api/products/[id].js
@@ -5,27 +5,34 @@ export default async function handler(request, response) {
   await dbConnect();
   const { id } = request.query;
 
-  if (request.method === "GET") {
-    const product = await Product.findById(id).populate("reviews");
+  try {
+    if (request.method === "GET") {
+      const product = await Product.findById(id).populate("reviews");
 
-    if (!product) {
-      return response.status(404).json({ status: "Not Found" });
+      if (!product) {
+        return response.status(404).json({ status: "Not Found" });
+      }
+
+      response.status(200).json(product);
     }
 
-    response.status(200).json(product);
-  }
+    if (request.method === "PUT") {
+      const updatedProduct = request.body;
 
-  if (request.method === "PUT") {
-    const updatedProduct = request.body;
+      await Product.findByIdAndUpdate(id, updatedProduct);
 
-    await Product.findByIdAndUpdate(id, updatedProduct);
+      response.status(200).json({ status: "Product successfully updated." });
+    }
 
-    response.status(200).json({ status: "Product successfully updated." });
-  }
-  if (request.method === "DELETE") {
-    await Product.findByIdAndDelete(id);
-    response
-      .status(200)
-      .json({ status: `Product ${id} successfully deleted.` });
+    if (request.method === "DELETE") {
+      await Product.findByIdAndDelete(id);
+      response
+        .status(200)
+        .json({ status: `Product ${id} successfully deleted.` });
+    }
+  } catch (error) {
+    console.log(error);
+    response.status(500).json({ message: "Internal Server error" });
+    return;
   }
 }
